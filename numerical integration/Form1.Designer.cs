f0f69229e20977169046e0bf2bf1cf1cb843f743
Adil Akhmetov <aa13081@auca.kg>
2026-03-11 22:29:24 +0600

    Добавьте файлы проекта.

---
+++ numerical integration/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace numerical_integration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl = new ZedGraph.ZedGraphControl();
+            this.problem = new System.Windows.Forms.ComboBox();
+            this.method = new System.Windows.Forms.ComboBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.number_of_nodes = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.integral = new System.Windows.Forms.Label();
+            this.error = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl
+            // 
+            this.zedGraphControl.Location = new System.Drawing.Point(372, 12);
+            this.zedGraphControl.Name = "zedGraphControl";
+            this.zedGraphControl.ScrollGrace = 0D;
+            this.zedGraphControl.ScrollMaxX = 0D;
+            this.zedGraphControl.ScrollMaxY = 0D;
+            this.zedGraphControl.ScrollMaxY2 = 0D;
+            this.zedGraphControl.ScrollMinX = 0D;
+            this.zedGraphControl.ScrollMinY = 0D;
+            this.zedGraphControl.ScrollMinY2 = 0D;
+            this.zedGraphControl.Size = new System.Drawing.Size(1064, 562);
+            this.zedGraphControl.TabIndex = 0;
+            // 
+            // problem
+            // 
+            this.problem.AccessibleDescription = "";
+            this.problem.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.problem.FormattingEnabled = true;
+            this.problem.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.problem.Location = new System.Drawing.Point(26, 60);
+            this.problem.Name = "problem";
+            this.problem.Size = new System.Drawing.Size(309, 39);
+            this.problem.TabIndex = 1;
+            // 
+            // method
+            // 
+            this.method.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.method.FormattingEnabled = true;
+            this.method.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.method.Location = new System.Drawing.Point(26, 220);
+            this.method.Name = "method";
+            this.method.Size = new System.Drawing.Size(309, 39);
+            this.method.TabIndex = 2;
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.Start.Location = new System.Drawing.Point(26, 489);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(309, 85);
+            this.Start.TabIndex = 3;
+            this.Start.Text = "Начать";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // number_of_nodes
+            // 
+            this.number_of_nodes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.number_of_nodes.Location = new System.Drawing.Point(26, 368);
+            this.number_of_nodes.Name = "number_of_nodes";
+            this.number_of_nodes.Size = new System.Drawing.Size(309, 38);
+            this.number_of_nodes.TabIndex = 4;
+            this.number_of_nodes.Tag = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label1.Location = new System.Drawing.Point(20, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Количество Узлов:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label2.Location = new System.Drawing.Point(20, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Метод:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label3.Location = new System.Drawing.Point(20, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 31);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Задача:";
+            // 
+            // integral
+            // 
+            this.integral.AutoSize = true;
+            this.integral.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.integral.Location = new System.Drawing.Point(20, 602);
+            this.integral.Name = "integral";
+            this.integral.Size = new System.Drawing.Size(0, 31);
+            this.integral.TabIndex = 8;
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.error.Location = new System.Drawing.Point(20, 656);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(0, 31);
+            this.error.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1457, 709);
+            this.Controls.Add(this.error);
+            this.Controls.Add(this.integral);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.number_of_nodes);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.method);
+            this.Controls.Add(this.problem);
+            this.Controls.Add(this.zedGraphControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl;
+        private System.Windows.Forms.ComboBox problem;
+        private System.Windows.Forms.ComboBox method;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox number_of_nodes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label integral;
+        private System.Windows.Forms.Label error;
+    }
+}
+
---
+++ numerical integration/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace numerical_integration
+{
+    public partial class Form1 : Form
+    {
+        int ind_of_problem, ind_of_method, num_of_nodes;
+        PointPairList nodes = new PointPairList();
+        double ans = 0, err = 0, a, b, step;
+        const double pi = Math.PI;
+        GraphPane pane;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pane = zedGraphControl.GraphPane;
+            pane.Title.Text = "График интерполяции";
+            pane.Title.FontSpec.Size = 16;
+            pane.XAxis.Title.Text = "Ось X";
+            pane.YAxis.Title.Text = "Ось Y";
+            zedGraphControl.GraphPane.XAxis.MajorGrid.IsVisible = true;
+            zedGraphControl.GraphPane.YAxis.MajorGrid.IsVisible = true;
+            zedGraphControl.AxisChange();
+            zedGraphControl.Invalidate();
+        }
+
+        private double f(double x)
+        {
+            switch (ind_of_problem)
+            {
+                case 0:
+                    return x * x * x;
+                case 1:
+                    return x / Math.Sqrt(1 - x * x);
+                case 2:
+                    return x/Math.Sqrt(4-x*x);
+                case 3:
+                    double a = 50;
+                    return (a / x) * (a / x) * Math.Sin(a / x);
+                case 4:
+                    return Math.Exp(-x * x);
+                default:
+                    return 0;
+            }
+            
+        }
+        private void init_nodes()
+        {
+            nodes.Clear();
+            switch (ind_of_problem)
+            {
+                case 0:
+                    a = 0;
+                    b = 2;
+                    break;
+                case 1:
+                    a = 0;
+                    b = 0.99;
+                    break;
+                case 2:
+                    a = 0;
+                    b = 1;
+                    break;
+                case 3:
+                    a = 1;
+                    b = 3;
+                    break;
+                case 4:
+                    a = 0;
+                    b = 5;
+                    break;
+            }
+
+            step = (b - a) / (num_of_nodes - 1);
+            nodes.Add(a, f(a));
+            for (int i = 1; i < num_of_nodes; i++)
+            {
+                double x = nodes.Last().X + step;
+                nodes.Add(x, f(x));
+            }
+        }
+        private void draw_function()
+        {
+            PointPairList list = new PointPairList();
+            double N = 10000 * (b - a);
+            double stp = (b - a)/ (N - 1), x = a;
+            for (int i = 0; i < N; i++)
+            {
+                list.Add(x, f(x));
+                x += stp;
+            }
+            LineItem myCurve = pane.AddCurve("Функция", list, Color.Red, SymbolType.None);
+            myCurve.Line.Width = 3.0F;
+        }
+        private void draw_nodes()
+        {
+            LineItem myCurve = pane.AddCurve("Узел", nodes, Color.Red, SymbolType.Circle);
+            myCurve.Line.IsVisible = false;
+            myCurve.Symbol.Fill = new Fill(Color.Blue);
+            myCurve.Line.Width = 8.0F;
+        }
+        private void draw_rectangles()
+        {           
+            for (int i = 0; i < num_of_nodes - 1; i++)
+            {
+                PointPairList list = new PointPairList();
+                if (ind_of_method == 0)
+                {
+                    list.Add(nodes[i].X, 0);
+                    list.Add(nodes[i].X, nodes[i].Y);
+                    list.Add(nodes[i + 1].X, nodes[i].Y);
+                    list.Add(nodes[i + 1].X, 0);
+                    list.Add(nodes[i].X, 0);
+                }
+                else if (ind_of_method == 1)
+                {
+                    list.Add(nodes[i].X, 0);
+                    list.Add(nodes[i].X, nodes[i + 1].Y);
+                    list.Add(nodes[i + 1].X, nodes[i + 1].Y);
+                    list.Add(nodes[i + 1].X, 0);
+                    list.Add(nodes[i].X, 0);
+                }
+                else
+                {
+                    list.Add(nodes[i].X, 0);
+                    list.Add(nodes[i].X, f((nodes[i].X + nodes[i + 1].X) / 2.0));
+                    list.Add(nodes[i + 1].X, f((nodes[i].X + nodes[i + 1].X) / 2.0));
+                    list.Add(nodes[i + 1].X, 0);
+                    list.Add(nodes[i].X, 0);
+                }
+                LineItem polygon = pane.AddCurve("", list, Color.Black, SymbolType.None);
+                polygon.Line.Fill = new Fill(Color.LightBlue);
+            }
+        }
+        private void draw_trapezoids()
+        {
+            for (int i = 0; i < num_of_nodes - 1; i++)
+            {
+                PointPairList list = new PointPairList();
+                
+                list.Add(nodes[i].X, 0);
+                list.Add(nodes[i].X, nodes[i].Y);
+                list.Add(nodes[i + 1].X, nodes[i+1].Y);
+                list.Add(nodes[i + 1].X, 0);
+                list.Add(nodes[i].X, 0);
+                
+                LineItem polygon = pane.AddCurve("", list, Color.Black, SymbolType.None);
+                polygon.Line.Fill = new Fill(Color.LightBlue);
+            }
+        }
+        private double L(double x, double l, double r)
+        {
+            return 2.0/((r-l)*(r-l))*((x-r)*(x-(l+r)/2.0)*f(l) - 2.0*(x-l)*(x-r)*f((l+r)/2.0) + (x-l)*(x-(l+r)/2.0)*f(r));
+        }
+        private void draw_interpolant()
+        {
+            PointPairList list = new PointPairList();
+            double N = 10000*(b-a);
+            double stp = (b - a) / (N - 1), x = a;
+            for (int i = 0; i < num_of_nodes - 1; i++)
+            {
+                while (x <= nodes[i + 1].X)
+                {
+                    list.Add(x, L(x, nodes[i].X, nodes[i+1].X));
+                    x += stp;
+                }
+                list.Add(nodes[i+1].X, L(nodes[i+1].X, nodes[i].X, nodes[i+1].X));
+            }
+            LineItem myCurve = pane.AddCurve("Интерполянт", list, Color.LightBlue, SymbolType.None);
+            myCurve.Line.Width = 3.0F;
+        }
+        private void calc()
+        {
+            ans = 0;
+            err = 0;
+            if (ind_of_method == 0){
+                for(int i = 0; i < num_of_nodes-1; i++)
+                {
+                    ans += step * nodes[i].Y;
+                }
+            }
+            else if (ind_of_method == 1)
+            {
+                for (int i = 0; i < num_of_nodes - 1; i++)
+                {
+                    ans += step * nodes[i + 1].Y;
+                }
+            }
+            else if(ind_of_method == 2)
+            {
+                for (int i = 0; i < num_of_nodes - 1; i++)
+                {
+                    ans += step * f((nodes[i].X + nodes[i + 1].X) / 2.0);
+                }
+            }
+            else if (ind_of_method == 3)
+            {
+                for (int i = 0; i < num_of_nodes - 1; i++)
+                {
+                    ans += step*(nodes[i].Y + nodes[i+1].Y)/2.0;
+                }
+            }
+            else if (ind_of_method == 4)
+            {
+                for (int i = 0; i < num_of_nodes - 1; i++)
+                {
+                    ans += step * (nodes[i].Y + 4.0 * f((nodes[i].X + nodes[i + 1].X) / 2.0) + nodes[i+1].Y) / 6.0;
+                }
+            }
+        }
+        private void draw()
+        {
+            pane.CurveList.Clear();
+            zedGraphControl.AxisChange();
+            zedGraphControl.Invalidate();
+            draw_nodes();
+            if (ind_of_method == 4) draw_interpolant();
+            draw_function();
+            if (ind_of_method == 3) draw_trapezoids();
+            else if(ind_of_method <= 2) draw_rectangles();
+            zedGraphControl.AxisChange();
+            zedGraphControl.Invalidate();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ind_of_problem = problem.SelectedIndex;
+            ind_of_method = method.SelectedIndex;
+            num_of_nodes = Convert.ToInt32(number_of_nodes.Text);
+            init_nodes();
+            calc();
+            draw();
+            integral.Text = "Площадь под графиком: " + ans.ToString("F5");
+            error.Text = "Пошрешность: " + err.ToString("F5");
+        }
+
+    }
+}
